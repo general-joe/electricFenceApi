47f424d04c6dc0f7fab7ba8b45a6787ba113170f
D-General <kwabijoe274@gmail.com>
2024-06-07 08:31:51 +0000

    Installed necessary packages for the project

---
+++ src/middlewares/userCheck.js
@@ -0,0 +1,22 @@
+const prisma = require("@prisma/client");
+const httpstatus = require("../utils/httpStatusCode");
+
+exports.user = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const checkUser = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (checkUser) {
+      res
+        .status(httpstatus.BADREQUEST)
+        .json({ message: "User has already registered" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
