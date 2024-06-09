f45e10b5fcbb86985fb2ea35b7cf06ca8e3cafc2
python <oklement3@gmail.com>
2024-06-09 14:40:55 +0000

    added a custom Error and completed the crud for the user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,8 @@
 const prisma = require("../utils/prismaUtil");
 const bcrypt = require("../utils/bcrypt");
+const httpstatus = require("../utils/httpStatusCode")
+const { signToken } = require("../utils/tokenUtil");
+
 const {
   signUp,
   getUsers,
@@ -15,20 +18,86 @@ exports.addUser = async (req, res, next) => {
     data.password = bcrypt.hash(data.password);
     const user = await signUp(data);
     delete user.password;
-    res.status(200).json({ message: "user created successfully" });
+    res.status(httpstatus.CREATED).json({ message: "user created successfully" });
   } catch (error) {
     console.log(error);
+    next(new CustomError(500, error));
   }
 };
 
-exports.Users = async(req,res,next)=>{
+exports.listUsers = async(req,res,next)=>{
   try{
-
+    const users = await getUsers();
+    res.status(httpstatus.OK).json({users})
   }catch(error){
     console.log(error);
+    next(new CustomError(500, error));
   }
 }
 
-// module.exports = {
-//   addUser,
-// };
+exports.findUserbyId = async(req,res,next)=>{
+  try{
+    const {id} = req.params
+  const user = await getUserById(id);
+  res.status(httpstatus.OK).json({user})
+
+  }catch(error){
+    console.log(error)
+    next(new CustomError(500, error));
+  }
+};
+
+exports.updateUser = async(req,res,next)=>{
+  try{
+    const id = req.params
+    const data = req.body
+    const update = await editUser(id, data)
+    res.status(httpstatus.OK).json({update})
+
+  }catch(error){
+    console.log(error)
+    next(new CustomError(500, error));
+  }
+};
+
+exports.deleteUser = async(req,res,next)=>{
+  try{
+    const id = req.params
+    const deleteUser = await removeUser(id)
+    res.status(httpstatus.OK).json({message: "user removed successfully"})
+  }catch(error){
+    console.log(error)
+    next(new CustomeError(500, error));
+  }
+};
+
+exports.signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const client = await login(email);
+    if (!client) {
+      throw new Error("email not found");
+    } else {
+      const checkPassword = await bcrypt.compare(password, client.password);
+
+      if (!checkPassword) {
+        throw new Error("Invalid credentials");
+      } else {
+        delete client.password;
+        const token = signToken(client.id);
+        res.status(httpstatus.OK).json({
+          message: "User succesfully logged in !",
+          username: client.username,
+          email: client.email,
+          token,
+          id: client.id,
+          role: client.role,
+        });
+      }
+    }
+  } catch (error) {
+    console.log(error)
+    next(new CustomError(500, error));
+  }
+};
+
